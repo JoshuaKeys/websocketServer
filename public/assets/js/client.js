0ecf74e12622e39cefa0750a16554d89fcae02e9
Joshua <joshua_oguma@epam.com>
2021-02-18 14:31:17 +0200

    added webrtc video

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -225,8 +225,7 @@ function loginProcess(success) {
   if (success === false) {
     alert('Try a different username');
   } else {
-    alert('Connected');
-    navigator.getUserMedia(
+    navigator.mediaDevices.getUserMedia(
       {
         video: true,
         audio: true,
