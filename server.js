4390ca5382af04312245d55cd0107faf550deb3c
Joshua <joshua_oguma@epam.com>
2021-02-18 14:01:46 +0200

    making changes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
   console.log('Hello World');
+  res.send('Hello World');
 });
 const server = http.createServer(app);
 
