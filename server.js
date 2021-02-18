958774e9d949b9d5d0d3b9d5b54c957cc01a38ff
Joshua <joshua_oguma@epam.com>
2021-02-18 14:13:59 +0200

    added public folder

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var http = require('http');
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) => {
-  console.log('Hello World');
-  res.send('Hello World');
-});
+app.use(express.static('public'));
+// app.get('/', (req, res) => {
+//   console.log('Hello World');
+//   res.send('Hello World');
+// });
 const server = http.createServer(app);
 
 server.listen(3000, () => {
