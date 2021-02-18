d1f365eb3c535f4e70620b8eba3aa9867758209b
Joshua <joshua_oguma@epam.com>
2021-02-18 13:10:30 +0200

    made more changes

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 
 const server = http.createServer(app);
+app.get('/test', (req, res) => {
+  res.send('Hello World');
+});
 server.listen(3000, () => {
   console.log('App listening at port 3000');
 });
