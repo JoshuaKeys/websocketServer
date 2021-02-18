65a26cbb1c6f29778d4fb6d51b1be37653db56cb
Joshua <joshua_oguma@epam.com>
2021-02-18 14:49:24 +0200

    making corrections

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -315,7 +315,7 @@ function loginProcess(success) {
       })
       .catch(function (error) {
         console.log(error);
-        alert('Error', error);
+        alert('Error', JSON.stringify(error));
       });
   }
 }
