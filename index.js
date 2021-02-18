a609adcaf2c5f7dec179f898533527ab3d454a90
Joshua <joshua_oguma@epam.com>
2021-02-18 12:31:33 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const httpServer = require('./server');
+const webSocketServ = require('websocket').server;
+const handleWsMsg = require('./lib/helpers/handleWsMsg');
+var wss = new webSocketServ({
+  httpServer,
+});
+
+var users = {};
+var otherUser;
+function originIsAllowed(origin) {
+  return true;
+}
+
+wss.on('request', (request) => {
+  if (!originIsAllowed(request.origin)) {
+    request.reject();
+    return;
+  }
+  var conn = request.accept('echo-protocol', request.origin);
+
+  conn.on('message', function (message) {
+    var data;
+    try {
+      data = JSON.parse(message.utf8Data);
+      console.log(data);
+    } catch (e) {
+      console.log('Invalid JSON');
+      data = {};
+    }
+    handleWsMsg(data, conn, users);
+  });
+
+  conn.on('close', function () {
+    console.log('Connection closed');
+    if (conn.name) {
+      delete users[conn.name];
+      if (conn.otherUser) {
+        var connect = users[conn.otherUser];
+        conn.otherUser = null;
+
+        if (conn != null) {
+          sendToOtherUser(connect, {
+            type: 'leave',
+          });
+        }
+      }
+    }
+  });
+
+  conn.send(JSON.stringify({ msg: 'Hello World' }));
+});
+
+wss.on('connection', function (conn) {
+  console.log('User connected');
+});
---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+const express = require('express');
+
+const app = express();
+
+const server = http.createServer(app);
+server.listen(3000, () => {
+  console.log('App listening at port 3000');
+});
+
+module.exports = server;
